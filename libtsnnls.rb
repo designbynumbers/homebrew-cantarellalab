c4c15cff348048e850149b7e999f1f20efe65961
Jason Cantarella <jason.cantarella@gmail.com>
2022-09-28 14:16:13 -0400

    minor tweak

--- libtsnnls.rb
+++ libtsnnls.rb
@@ -17,7 +17,7 @@ class Libtsnnls < Formula
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules","LDFLAGS=\"-L/opt/homebrew/opt/openblas/lib/\"","CPPFLAGS=\"-I/opt/homebrew/opt/openblas/include"
+    system "./configure", *std_configure_args, "--disable-silent-rules","LDFLAGS=\"-L/opt/homebrew/opt/openblas/lib/\"","CPPFLAGS=\"-I/opt/homebrew/opt/openblas/include\""
     
     system "make", "install"
   end
